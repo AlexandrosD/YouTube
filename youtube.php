2352b5921e592d5af3285c20ff8e63b8f21c7170
Alexandros D <alexd3499@gmail.com>
2011-09-17 10:07:05 +0000

    load favorites correction

--- youtube.php
+++ youtube.php
@@ -207,17 +207,6 @@ class YouTube {
 		return $this->_httpGet($url);
 	}
 	
-	/**
-	 * Get a single video's data
-	 * 
-	 * @param String $videoId The video ID
-	 * @return String 
-	 */
-	public function getVideo( $videoId ) {
-		$url = $this->_baseUrl . "api/videos/" . $videoId;
-		return $this->_httpGet($url);
-	}
-	
 	/***************************************************************************
 	****************************************************************************
 	*** Private functions
